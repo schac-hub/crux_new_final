1582c238f5aa0ce9a84b17fbd3845f3d03a07432
SCHAC <kouakouchristevann@gmail.com>
2026-09-11 00:00:55 +0000

    Add Material Components library dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.multidex:multidex:2.0.1")
 }
 
